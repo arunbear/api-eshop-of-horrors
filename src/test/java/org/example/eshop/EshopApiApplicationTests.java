828bef864e05158806d16a3355e400f310fba593
Arun Prasaad <arunbear@cpan.org>
2024-08-30 11:09:30 +0100

    Add test T1 for product creation with name and price

--- src/test/java/org/example/eshop/EshopApiApplicationTests.java
+++ src/test/java/org/example/eshop/EshopApiApplicationTests.java
@@ -1,13 +1,56 @@
 package org.example.eshop;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.assertj.core.api.Assertions;
+import org.example.eshop.dto.ProductDto;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.matchesRegex;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class EshopApiApplicationTests {
+    @LocalServerPort
+    private int localServerPort;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.port = localServerPort;
+    }
 
     @Test
-    void contextLoads() {
+    void returns_details_of_a_new_product_on_POST_to_products() throws JSONException {
+        var productToCreate = new JSONObject()
+            .put("name", "Special Smelly Cheese")
+            .put("price", 20.99);
+
+        ProductDto product = RestAssured
+            .given()
+            .contentType(ContentType.JSON)
+            .body(productToCreate.toString())
+            .post("/products")
+            .then()
+            .statusCode(equalTo(HttpStatus.SC_CREATED))
+            .header("Location", matchesRegex(".+/products/[1-9][0-9]*"))
+            .extract()
+            .as(ProductDto.class);
+
+        assertThat(product.name())
+            .isEqualTo(productToCreate.getString("name"));
+        assertThat(product.price())
+            .isEqualTo(productToCreate.getDouble("price"));
+        assertThat(product.id())
+            .isGreaterThan(0);
     }
 
 }
